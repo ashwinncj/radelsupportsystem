ceb7d3cc2db627357bfe2a1ea9e05918ce836235
ashwinncj <30401526+ashwinncj@users.noreply.github.com>
2018-12-14 23:28:52 +0530

    initial

---
+++ form.php
@@ -0,0 +1,13 @@
+<?php
+// required headers
+//header("Access-Control-Allow-Origin: *");
+//header("Content-Type: application/json; charset=UTF-8");
+?>
+<html>
+<head>
+
+</head>
+<body>
+
+</body>
+</html>
