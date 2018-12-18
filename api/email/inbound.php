caa72681cc65b78abfe868aa17f5ca08fc83b399
ashwinncj <30401526+ashwinncj@users.noreply.github.com>
2018-12-18 13:00:40 +0530

    Update inbound.php

--- api/email/inbound.php
+++ api/email/inbound.php
@@ -1,5 +1,6 @@
 <?php
 
+isset($_POST) ? TRUE : exit('No POST Set !');
 $params['user_email'] = 'ash@test';
 $params['user_name'] = 'Ash';
 $params['user_message'] = 'Message';
@@ -7,7 +8,8 @@ $params['user_message'] = 'Message';
 //$params['user_name'] = $_POST['from'];
 //$params['user_message'] = $_POST['stripped-text'];
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "localhost/radelsupportsystem/api/ticket/create.php");
+curl_setopt($ch, CURLOPT_URL, "http://radel.space/api/ticket/create.php");
+//curl_setopt($ch, CURLOPT_URL, "localhost/radelsupportsystem/api/ticket/create.php");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
