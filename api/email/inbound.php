807f1cfb16d54a22f37381fb2d87c2d2849932e9
ashwinncj <30401526+ashwinncj@users.noreply.github.com>
2018-12-19 16:43:27 +0530

    Update

--- api/email/inbound.php
+++ api/email/inbound.php
@@ -7,7 +7,7 @@ $params['user_email'] = $_POST['sender'];
 $params['user_name'] = $_POST['from'];
 $params['user_message'] = $_POST['stripped-text'];
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://radel.space/api/ticket/create.php");
+curl_setopt($ch, CURLOPT_URL, "http://radel.space/radelsupportsystem/api/ticket/create.php");
 //curl_setopt($ch, CURLOPT_URL, "localhost/radelsupportsystem/api/ticket/create.php");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
--- form.php
+++ form.php
@@ -4,7 +4,7 @@ if (isset($_POST['user_email'])) {
     $params['user_name'] = $_POST['user_name'];
     $params['user_message'] = $_POST['user_message'];
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "http://radel.space/api/ticket/create.php");
+    curl_setopt($ch, CURLOPT_URL, "http://radel.space/radelsupportsystem/api/ticket/create.php");
     //curl_setopt($ch, CURLOPT_URL, "localhost/radelsupportsystem/api/ticket/create.php");
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
